3ee245b2133ead992af5cbbc30fd7580165f7539
jacquelinesnook <jasnook@ntlworld.com>
2017-11-03 17:19:33 +0000

    Changes for getting bank accounts

--- grails-app/domain/com/rhindon/bridge/Instance.groovy
+++ grails-app/domain/com/rhindon/bridge/Instance.groovy
@@ -11,27 +11,10 @@ class Instance {
     String entryConfirmationEmailAddresses
     String entryConfirmationSignature
     String heatUpdatedEmailAddresses
-    String stripeApiKey
-    String stripeApiTestKey
-    boolean stripeProductionMode
     boolean swallowOnlineCosts
-    String bankAccountNumber
-    String sortCode
-    String chequePayee
-    String chequeAddress
-    String bannerColour
 
-//    static constraints = {
-//        name nullable: false
-//        entryConfirmationEmailAddresses nullable: true
-//        heatUpdatedEmailAddresses nullable: true
-//        stripeApiKey nullable: true
-//        stripeApiTestKey nullable: true
-//        chequePayee nullable: true
-//        chequeAddress nullable: true
-//    }
-
-    String getActiveStripeKey() {
-        return stripeProductionMode ? stripeApiKey : stripeApiTestKey;
+    static constraints = {
+        entryConfirmationEmailAddresses nullable: true
+        heatUpdatedEmailAddresses nullable: true
     }
 }
