96d4d2edf847554a5a05f11013e1667fd2007e0e
Jacqui Snook <jacqui.snook@theexchangelab.com>
2017-11-14 10:28:49 +0000

    Add eccles cup points classes

---
+++ src/test/groovy/com/rhindon/bridge/view/EcclesCupPointsSpec.groovy
@@ -0,0 +1,18 @@
+package com.rhindon.bridge.view
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class EcclesCupPointsSpec extends Specification implements DomainUnitTest<EcclesCupPoints> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
